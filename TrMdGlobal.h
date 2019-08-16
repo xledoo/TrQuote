ed64cc79893253692796214ac965f46c8eaa7f84
xledoo <xledoo@qq.com>
2019-08-17 01:49:29 +0800

    upgrade

---
+++ TrMdGlobal.h
@@ -0,0 +1,33 @@
+#ifndef TRMDGLOBAL_H
+#define TRMDGLOBAL_H
+
+#include <QtCore/qglobal.h>
+
+#if defined(TRQUOTE_LIBRARY)
+#  define TRQUOTE_EXPORT Q_DECL_EXPORT
+#else
+#  define TRQUOTE_EXPORT Q_DECL_IMPORT
+#endif
+
+enum class TrMdResponseId
+{
+    TrMarketUserLogin			= 100,
+    TrOnRspUserLogin            = 101,
+    TrOnRspUserLogout           = 102,
+    TrOnRtnDepthMarketData		= 103
+};
+
+
+class CTrMdReceiver
+{
+public:
+    virtual void OnReceiveMdData(TrMdResponseId rId, void* wParam, void* lParam) = 0;
+};
+
+class CTrMdService
+{
+public:
+    virtual void OnResponse(TrMdResponseId rId, void* wParam, void* lParam) = 0;
+};
+
+#endif // TRMDGLOBAL_H
---
+++ TrQuote.h
@@ -0,0 +1,22 @@
+﻿#ifndef TRQUOTE_H
+#define TRQUOTE_H
+
+#include "TrMdGlobal.h"
+
+
+class TRQUOTE_EXPORT CTrQuote
+{
+public:
+    CTrQuote();
+    // 服务登录 1.行情自动订阅 2.批量登录
+    void TrMarketStart();
+    // 服务登出 1.行情自动退订 2.批量登出
+    void TrMarketStop();
+    // 订阅合约 请在 TrMarketStart() 前添加订阅合约
+    void TrAddSubscribe(char* contract);
+    // 添加接收器
+    void TrAddReceiver(CTrMdReceiver* receiver);
+    // 删除接收器
+    void TrDelReceiver(CTrMdReceiver* receiver);
+};
+#endif // TRQUOTE_H
